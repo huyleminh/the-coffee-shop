ec87ce234cfc479576682d5a76d04ac4b54d9fbd
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2021-06-11 22:10:46 +0700

    Set up: base folder, mvc, cors config for server

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
+
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello, this is coffee shop server");
+    res.send("Hello world");
 });
 
 app.listen(PORT, () => {
