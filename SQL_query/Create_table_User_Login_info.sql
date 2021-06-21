1363b42db039cfe4d9af3cc9e985e85416e9c21f
binhhieutu <63572337+binhhieutu@users.noreply.github.com>
2021-06-21 17:19:42 +0700

    update query create user table
    change colum name, change type to store Unicode, check FK

--- SQL_query/Create_table_User_Login_info.sql
+++ SQL_query/Create_table_User_Login_info.sql
@@ -5,20 +5,14 @@ USE `heroku_a51da3167c7e5af` ;
 -- Table `heroku_a51da3167c7e5af`.`user_info`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`user_info` (
-  `id_user` CHAR(8) NOT NULL,
+  `ID` CHAR(8) NOT NULL,
   `name` NVARCHAR(50) NULL DEFAULT NULL,
   `Address` NVARCHAR(150) NULL DEFAULT NULL,
   `Phone_number` VARCHAR(12) NOT NULL,
   `gender` BIT(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_user`))
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-SELECT `user_info`.`id_user`,
-    `user_info`.`name`,
-    `user_info`.`Address`,
-    `user_info`.`Phone_number`,
-    `user_info`.`gender`
-FROM `heroku_a51da3167c7e5af`.`user_info`;
 
 -- -----------------------------------------------------
 -- Table `heroku_a51da3167c7e5af`.`user_login`
@@ -27,17 +21,18 @@ CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`user_login` (
   `ID` CHAR(8) NOT NULL DEFAULT '',
   `username` VARCHAR(50) NOT NULL,
   `password` VARCHAR(50) NOT NULL,
-  `role_id` SMALLINT(6) NULL DEFAULT NULL,
-  `Create_AT` timestamp NOT NULL,
+  `role_id` SMALLINT(1) NULL DEFAULT NULL,
+  `Created_AT` timestamp NOT NULL,
   `Updated_At` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`ID`),
   CONSTRAINT `fk_user_login_user_info`
     FOREIGN KEY (`ID`)
-    REFERENCES `heroku_a51da3167c7e5af`.`user_info` (`id_user`)
+    REFERENCES `heroku_a51da3167c7e5af`.`user_info` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-
+SELECT* from  `user_info`
+join `user_login` on `user_info`.`ID`=`user_login`.`ID`;
 
