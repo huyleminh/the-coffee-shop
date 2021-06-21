399aac5b2e67b3b4ac9254f653b1c041b4010e69
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2021-06-21 22:52:17 +0700

    feature - sign up UI - done

--- client/src/views/layouts/Signup.jsx
+++ client/src/views/layouts/Signup.jsx
@@ -1,9 +1,71 @@
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useHistory } from "react-router-dom";
+import "../../assets/css/signup.css";
 
-Signup.propTypes = {};
+function Signup() {
+    const history = useHistory();
 
-function Signup(props) {
-    return <div>Sign up page</div>;
+    const handleSubmit = (e) => {
+        console.log("clicked");
+    };
+
+    const handleGoHome = () => {
+        history.push("/");
+    };
+
+    return (
+        <div className="signup">
+            <div className="form">
+                <div onClick={handleGoHome}>
+                    <FontAwesomeIcon icon={faHome} />
+                </div>
+
+                <div className="form__title">
+                    <h1>Sign Up</h1>
+                    <span>Become a member of our coffee shop</span>
+                </div>
+
+                <div className="form__controll">
+                    <form action="">
+                        <input
+                            type="text"
+                            placeholder="Fulllname"
+                            autoFocus={true}
+                            name="fullname"
+                            required
+                        />
+                        <input type="text" placeholder="Phone number" name="phone" required />
+                        <input type="text" placeholder="Address" name="address" required />
+                        <input type="text" placeholder="Username" name="username" required />
+                        <input type="password" placeholder="Password" name="password" required />
+                        <input
+                            type="password"
+                            placeholder="Confirm password"
+                            name="confirm"
+                            required
+                        />
+
+                        <span style={{ textAlign: "center" }}>
+                            Already have an account? <a href="/login">Login</a>
+                        </span>
+                        <button type="submit" onClick={handleSubmit}>
+                            SIGNUP
+                        </button>
+                    </form>
+                </div>
+
+                <div className="policy">
+                    <span>
+                        By clicking up the SIGNUP button, you agree to our
+                        <br />
+                        <a href="#">Terms &amp; Conditions</a> and <a href="#">Privacy Policy</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Signup;
