8a901f66a94f4b708405c365c886b69c21061e0e
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-06-26 11:35:44 +0700

    SQL_query/fixbug - recreate database - done

---
+++ SQL_query/createTable.sql
@@ -0,0 +1,41 @@
+CREATE SCHEMA IF NOT EXISTS `heroku_a51da3167c7e5af` DEFAULT CHARACTER SET utf8;
+USE `heroku_a51da3167c7e5af`;
+
+-- -----------------------------------------------------
+-- Table `heroku_a51da3167c7e5af`.`user_info`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`USER_INFO` (
+  `id` CHAR(8) NOT NULL,
+  `fullname` VARCHAR(50) NOT NULL,
+  `address` VARCHAR(150) NOT NULL,
+  `phoneNumber` VARCHAR(10) NOT NULL,
+  `gender` BIT(1) NOT NULL,
+  `updatedAt` TIMESTAMP NOT NULL,
+  `createdAt` TIMESTAMP NULL,
+  
+  CONSTRAINT `PK_USER_INFO` PRIMARY KEY (`id`),
+  CONSTRAINT `UC_USER_INFO` UNIQUE (`phoneNumber`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `heroku_a51da3167c7e5af`.`user_login`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`USER_LOGIN` (
+  `id` CHAR(8) NOT NULL,
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `roleID` SMALLINT(1) NOT NULL DEFAULT 3,
+  `updatedAT` TIMESTAMP NOT NULL,
+  
+  CONSTRAINT `PK_USER_LOGIN` PRIMARY KEY (`id`),
+  CONSTRAINT `UC_USER_LOGIN` UNIQUE (`username`),
+  CONSTRAINT `FK_USER_LONG_USER_INFO` FOREIGN KEY (`id`)
+    REFERENCES `heroku_a51da3167c7e5af`.`USER_INFO` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
---
+++ SQL_query/insertData.sql
@@ -0,0 +1,32 @@
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
+VALUES ('ABCDEFG1', 'Phan Minh Hiếu', 'TPHCM', '0123456789', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
+VALUES ('ABCDEFG1', 'taikhoan_bian1', '123456haha', '3', current_timestamp());
+
+
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
+VALUES ('ABCDEFG2', 'Lê Minh Huy', 'Gia Lai', '0987654321', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
+VALUES ('ABCDEFG2', 'taikhoan_andanh1', 'hihi123456', '3', current_timestamp());
+
+
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
+VALUES ('ABCDEFG3', 'Bạch Minh Khôi', 'Khánh Hoà', '0135792468', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
+VALUES ('ABCDEFG3', 'nhanvien_1', 'matkhaunhanvien1', '2', current_timestamp());
+
+
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
+VALUES ('ABCDEFG4', 'Nguyễn Hứa Hùng', 'TPHCM', '0246813579', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
+VALUES ('ABCDEFG4', 'nhanvien_2', 'matkhaunhanvien2', '2', current_timestamp());
+
+
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
+VALUES ('ABCDEFG5', 'Lê Hoàng Anh', 'Đồng Tháp', '0123167841', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
+VALUES ('ABCDEFG5', 'hackerne123', 'matkhau1khoqua', '1', current_timestamp());
+
+
+SELECT * FROM heroku_a51da3167c7e5af.USER_INFO;
+SELECT * FROM heroku_a51da3167c7e5af.USER_LOGIN;
