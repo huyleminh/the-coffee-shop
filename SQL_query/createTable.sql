77c42be14d2f62b845320339cdf8552202f87278
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-06-27 12:15:44 +0700

    change attributes name and input data

--- SQL_query/createTable.sql
+++ SQL_query/createTable.sql
@@ -5,7 +5,7 @@ USE `heroku_a51da3167c7e5af`;
 -- Table `heroku_a51da3167c7e5af`.`user_info`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`USER_INFO` (
-  `id` CHAR(8) NOT NULL,
+  `id` CHAR(7) NOT NULL,
   `fullname` VARCHAR(50) NOT NULL,
   `address` VARCHAR(150) NOT NULL,
   `phoneNumber` VARCHAR(10) NOT NULL,
@@ -24,11 +24,11 @@ DEFAULT CHARACTER SET = utf8;
 -- Table `heroku_a51da3167c7e5af`.`user_login`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`USER_LOGIN` (
-  `id` CHAR(8) NOT NULL,
+  `id` CHAR(7) NOT NULL,
   `username` VARCHAR(50) NOT NULL,
   `password` VARCHAR(50) NOT NULL,
-  `roleID` SMALLINT(1) NOT NULL DEFAULT 3,
-  `updatedAT` TIMESTAMP NOT NULL,
+  `role` SMALLINT(1) NOT NULL DEFAULT 3,
+  `updatedAt` TIMESTAMP NOT NULL,
   
   CONSTRAINT `PK_USER_LOGIN` PRIMARY KEY (`id`),
   CONSTRAINT `UC_USER_LOGIN` UNIQUE (`username`),
--- SQL_query/insertData.sql
+++ SQL_query/insertData.sql
@@ -1,31 +1,31 @@
 INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEFG1', 'Phan Minh Hiếu', 'TPHCM', '0123456789', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
-VALUES ('ABCDEFG1', 'taikhoan_bian1', '123456haha', '3', current_timestamp());
+VALUES ('ABCDEF1', 'Phan Minh Hiếu', 'TPHCM', '0123456789', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
+VALUES ('ABCDEF1', 'customer01', '1234567c', '3', current_timestamp());
 
 
 INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEFG2', 'Lê Minh Huy', 'Gia Lai', '0987654321', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
-VALUES ('ABCDEFG2', 'taikhoan_andanh1', 'hihi123456', '3', current_timestamp());
+VALUES ('ABCDEF2', 'Lê Minh Huy', 'Gia Lai', '0987654321', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
+VALUES ('ABCDEF2', 'customer02', '1234567c', '3', current_timestamp());
 
 
 INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEFG3', 'Bạch Minh Khôi', 'Khánh Hoà', '0135792468', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
-VALUES ('ABCDEFG3', 'nhanvien_1', 'matkhaunhanvien1', '2', current_timestamp());
+VALUES ('ABCDEF3', 'Bạch Minh Khôi', 'Khánh Hoà', '0135792468', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
+VALUES ('ABCDEF3', 'employee01', '1234567e', '2', current_timestamp());
 
 
 INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEFG4', 'Nguyễn Hứa Hùng', 'TPHCM', '0246813579', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
-VALUES ('ABCDEFG4', 'nhanvien_2', 'matkhaunhanvien2', '2', current_timestamp());
+VALUES ('ABCDEF4', 'Nguyễn Hứa Hùng', 'TPHCM', '0246813579', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
+VALUES ('ABCDEF4', 'employee02', '1234567e', '2', current_timestamp());
 
 
 INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEFG5', 'Lê Hoàng Anh', 'Đồng Tháp', '0123167841', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `roleID`, `updatedAt`) 
-VALUES ('ABCDEFG5', 'hackerne123', 'matkhau1khoqua', '1', current_timestamp());
+VALUES ('ABCDEF5', 'Lê Hoàng Anh', 'Đồng Tháp', '0123167841', b'0', current_timestamp(), current_timestamp());
+INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
+VALUES ('ABCDEF5', 'admin_01', '1234567a', '1', current_timestamp());
 
 
 SELECT * FROM heroku_a51da3167c7e5af.USER_INFO;
