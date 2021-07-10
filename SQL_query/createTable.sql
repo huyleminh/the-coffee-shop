48b0ba80acf3c9f410ec9c01813c8c71bf2dd420
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-07-10 22:48:15 +0700

    recreate tables involve product

--- SQL_query/createTable.sql
+++ SQL_query/createTable.sql
@@ -4,7 +4,7 @@ USE `heroku_a51da3167c7e5af`;
 -- -----------------------------------------------------
 -- Table `heroku_a51da3167c7e5af`.`user_info`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`USER_INFO` (
+CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`user_info` (
   `id` CHAR(7) NOT NULL,
   `fullname` VARCHAR(50) NOT NULL,
   `address` VARCHAR(150) NOT NULL,
@@ -23,7 +23,7 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `heroku_a51da3167c7e5af`.`user_login`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`USER_LOGIN` (
+CREATE TABLE IF NOT EXISTS `heroku_a51da3167c7e5af`.`user_login` (
   `id` CHAR(7) NOT NULL,
   `username` VARCHAR(50) NOT NULL,
   `password` VARCHAR(50) NOT NULL,
@@ -46,99 +46,41 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 CREATE TABLE `heroku_a51da3167c7e5af`.`product` (
   `id` VARCHAR(36) NOT NULL,
-  `name` NVARCHAR(150) NULL,
-  `image` TEXT NULL,
-  `price` INT NULL,
-  `description` TEXT NULL,
-  `discountId` VARCHAR(36) NULL,
-  `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` NVARCHAR(150) NOT NULL,
+  `image` TEXT DEFAULT NULL,
+  `price` INT DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
+  `discountId` VARCHAR(36) DEFAULT NULL,
+  `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+  CONSTRAINT `PK_PRODUCT` PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-
--- -----------------------------------------------------
--- Table `heroku_a51da3167c7e5af`.`product_category`product_category
--- -----------------------------------------------------
-CREATE TABLE `heroku_a51da3167c7e5af`.`product_category` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `categoryId` VARCHAR(36) NOT NULL,
-  `productId` VARCHAR(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `FK_INDEX_PRODUCT` (`productId` ASC),
-  INDEX `FK_INDEX_CATEGORY` (`categoryId` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
 
 -- -----------------------------------------------------
 -- Table `heroku_a51da3167c7e5af`.`category`
 -- -----------------------------------------------------
 CREATE TABLE `heroku_a51da3167c7e5af`.`category` (
   `id` VARCHAR(36) NOT NULL,
-  `name` NVARCHAR(60) NULL,
-  PRIMARY KEY (`id`))
+  `name` NVARCHAR(60) NOT NULL,
+  CONSTRAINT `PK_CATEGORY` PRIMARY KEY (`id`)
+)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
--- --------------------------
--- ADD FOREIGN PRODUCT TO PRODUCT_CATEGORY
--- ----------------------
-  -- --------------------------
--- ADD FOREIGN CATEGORY TO PRODUCT_CATEGORY
--- ----------------------
-ALTER TABLE `heroku_a51da3167c7e5af`.`product_category` 
-DROP COLUMN `id`,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`categoryId`, `productId`),
-DROP INDEX `FK_INDEX_PRODUCT` ,
-ADD INDEX `fk_product_category_category1_idx` (`productId` ASC),
-DROP INDEX `FK_INDEX_CATEGORY` ,
-ADD INDEX `fk_product_category_product1_idx` (`categoryId` ASC),
-DROP INDEX `id_UNIQUE` ;
-;
-ALTER TABLE `heroku_a51da3167c7e5af`.`product_category` 
-ADD CONSTRAINT `fk_product_category_category1`
-  FOREIGN KEY (`categoryId`)
-  REFERENCES `heroku_a51da3167c7e5af`.`category` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_product_category_product1`
-  FOREIGN KEY (`productId`)
-  REFERENCES `heroku_a51da3167c7e5af`.`product` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-  
-  
 -- -----------------------------------------------------
--- Table `heroku_a51da3167c7e5af`.`category`
+-- Table `heroku_a51da3167c7e5af`.`product_category`
 -- -----------------------------------------------------
-CREATE TABLE `heroku_a51da3167c7e5af`.`product_rating` (
+CREATE TABLE `heroku_a51da3167c7e5af`.`product_category` (
+  `categoryId` VARCHAR(36) NOT NULL,
   `productId` VARCHAR(36) NOT NULL,
-  `totalStar` INT NULL,
-  `totalRating` INT NULL,
-  PRIMARY KEY (`productId`),
-  CONSTRAINT `FK_PRODUC_RATING`
-    FOREIGN KEY (`productId`)
-    REFERENCES `heroku_a51da3167c7e5af`.`product` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-    
--- ------------------------------------------------------
--- Table `heroku_a51da3167c7e5af`.`product_rating`
--- ------------------------------------------------------
-CREATE TABLE `heroku_a51da3167c7e5af`.`product_rating` (
-  `productId` VARCHAR(36) NOT NULL,
-  `totalStar` INT NULL,
-  `totalRating` INT NULL,
-  PRIMARY KEY (`productId`),
-  CONSTRAINT `FK_PRODUC_RATING`
-    FOREIGN KEY (`productId`)
-    REFERENCES `heroku_a51da3167c7e5af`.`product` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  CONSTRAINT `PK_PRODUCT_CATEGORY` PRIMARY KEY (`categoryId`, `productId`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 
 -- ------------------------------------------------------
 -- Table `heroku_a51da3167c7e5af`.`product_rating`
@@ -146,19 +88,45 @@ CREATE TABLE `heroku_a51da3167c7e5af`.`product_rating` (
 CREATE TABLE `heroku_a51da3167c7e5af`.`discount` (
   `id` VARCHAR(36) NOT NULL,
   `percent` FLOAT NULL,
-  `active` BIT NULL,
+  `active` BIT NOT NULL,
   `startDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `endDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FK_DISCOUNT`
-    FOREIGN KEY (`id`)
-    REFERENCES `heroku_a51da3167c7e5af`.`product` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  CONSTRAINT `PK_DISCOUNT` PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- ------------------------------------------------------
+-- Table `heroku_a51da3167c7e5af`.`product_rating`
+-- ------------------------------------------------------
+CREATE TABLE `heroku_a51da3167c7e5af`.`product_rating` (
+  `productId` VARCHAR(36) NOT NULL,
+  `totalStar` INT NULL,
+  `totalRating` INT NULL,
+  CONSTRAINT `PK_PRODUCT_RATING` PRIMARY KEY (`productId`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
     
 
+ALTER TABLE `heroku_a51da3167c7e5af`.`product_category`
+ADD CONSTRAINT FK_PRODUCT_CATEGORY_PRODUCT
+FOREIGN KEY (productID)
+REFERENCES `heroku_a51da3167c7e5af`.`product`(id);
 
+ALTER TABLE `heroku_a51da3167c7e5af`.`product_category`
+ADD CONSTRAINT FK_PRODUCT_CATEGORY_CATEGORY1
+FOREIGN KEY (categoryID)
+REFERENCES `heroku_a51da3167c7e5af`.`category`(id);
 
+ALTER TABLE `heroku_a51da3167c7e5af`.`product_rating`
+ADD CONSTRAINT FK_PRODUCT_RATING_PRODUCT
+FOREIGN KEY (productID)
+REFERENCES `heroku_a51da3167c7e5af`.`product`(id);
 
-
-
+ALTER TABLE `heroku_a51da3167c7e5af`.`product`
+ADD CONSTRAINT FK_PRODUCT_DISCOUNT
+FOREIGN KEY (discountID)
+REFERENCES `heroku_a51da3167c7e5af`.`discount`(id);
+    
