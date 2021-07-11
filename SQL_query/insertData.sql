651e94b717ae50cefcfd44d2702ea7613dcf00e9
Anh Le <62140372+anhle199@users.noreply.github.com>
2021-07-11 14:58:48 +0700

    Update insertData.sql

--- SQL_query/insertData.sql
+++ SQL_query/insertData.sql
@@ -1,16 +1,16 @@
 INSERT INTO `heroku_a51da3167c7e5af`.`user_info` (id, fullname, address, phoneNumber, gender, updatedAt, createdAt)
 VALUES
-	('2eHuNfS','Phan Minh Hiếu','TPHCM','0123456789',b'0',current_timestamp(),current_timestamp()),
-	('ALggysE','le minh huy','','0111111115',b'0',current_timestamp(), current_timestamp()),
-	('cchwDul','le minh huy','','0111111114',b'0',current_timestamp(), current_timestamp()),
-	('d5iGKPp','Nguyễn Hứa Hùng','TPHCM','0246813579',b'0',current_timestamp(), current_timestamp()),
-	('d71N9dN','lê hoàng anh','Đồng Tháp','0762672399',b'0',current_timestamp(), current_timestamp()),
-	('Er1E2t8','Bạch Minh Khôi','Khánh Hoà','0135792468',b'0',current_timestamp(), current_timestamp()),
-	('FDHVWyT','le minh huy','','0111111112',b'0',current_timestamp(), current_timestamp()),
-	('J6saKBZ','le minh huy','','0111111116',b'0',current_timestamp(), current_timestamp()),
-	('xSF4R5S','le minh huy','','0111111111',b'0',current_timestamp(), current_timestamp()),
-	('yli440d','Lê Minh Huy','Gia Lai','0987654321',b'0',current_timestamp(), current_timestamp()),
-	('zIFLqCG','le minh huy','','0111111113',b'0',current_timestamp(), current_timestamp());
+	('2eHuNfS', 'Phan Minh Hiếu', 'TPHCM', '0123456789', b'0', current_timestamp(), current_timestamp()),
+	('ALggysE', 'le minh huy', '', '0111111115', b'0', current_timestamp(), current_timestamp()),
+	('cchwDul', 'le minh huy', '', '0111111114', b'0', current_timestamp(), current_timestamp()),
+	('d5iGKPp', 'Nguyễn Hứa Hùng', 'TPHCM', '0246813579', b'0', current_timestamp(), current_timestamp()),
+	('d71N9dN', 'lê hoàng anh', 'Đồng Tháp', '0762672399', b'0', current_timestamp(), current_timestamp()),
+	('Er1E2t8', 'Bạch Minh Khôi',' Khánh Hoà', '0135792468', b'0', current_timestamp(), current_timestamp()),
+	('FDHVWyT', 'le minh huy', '', '0111111112', b'0', current_timestamp(), current_timestamp()),
+	('J6saKBZ', 'le minh huy', '', '0111111116', b'0', current_timestamp(), current_timestamp()),
+	('xSF4R5S', 'le minh huy', '', '0111111111', b'0', current_timestamp(), current_timestamp()),
+	('yli440d', 'Lê Minh Huy', 'Gia Lai', '0987654321', b'0', current_timestamp(), current_timestamp()),
+	('zIFLqCG', 'le minh huy', '', '0111111113', b'0', current_timestamp(), current_timestamp());
     
 INSERT INTO `heroku_a51da3167c7e5af`.`user_login` (id, username, password, role, updatedAt)
 VALUES
