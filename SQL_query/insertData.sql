f56a0186d75adce42ae046ac929b5892d312e417
Anh Le <62140372+anhle199@users.noreply.github.com>
2021-07-11 14:57:03 +0700

    Update insertData.sql

--- SQL_query/insertData.sql
+++ SQL_query/insertData.sql
@@ -1,32 +1,27 @@
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEF1', 'Phan Minh Hiếu', 'TPHCM', '0123456789', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
-VALUES ('ABCDEF1', 'customer01', '1234567c', '3', current_timestamp());
-
-
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEF2', 'Lê Minh Huy', 'Gia Lai', '0987654321', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
-VALUES ('ABCDEF2', 'customer02', '1234567c', '3', current_timestamp());
-
-
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEF3', 'Bạch Minh Khôi', 'Khánh Hoà', '0135792468', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
-VALUES ('ABCDEF3', 'employee01', '1234567e', '2', current_timestamp());
-
-
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEF4', 'Nguyễn Hứa Hùng', 'TPHCM', '0246813579', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
-VALUES ('ABCDEF4', 'employee02', '1234567e', '2', current_timestamp());
-
-
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_INFO` (`id`, `fullname`, `address`, `phoneNumber`, `gender`, `updatedAt`, `createdAt`) 
-VALUES ('ABCDEF5', 'Lê Hoàng Anh', 'Đồng Tháp', '0123167841', b'0', current_timestamp(), current_timestamp());
-INSERT INTO `heroku_a51da3167c7e5af`.`USER_LOGIN` (`id`, `username`, `password`, `role`, `updatedAt`) 
-VALUES ('ABCDEF5', 'admin_01', '1234567a', '1', current_timestamp());
-
-
-SELECT * FROM heroku_a51da3167c7e5af.USER_INFO;
-SELECT * FROM heroku_a51da3167c7e5af.USER_LOGIN;
+INSERT INTO `heroku_a51da3167c7e5af`.`user_info` (id, fullname, address, phoneNumber, gender, updatedAt, createdAt)
+VALUES
+	('2eHuNfS','Phan Minh Hiếu','TPHCM','0123456789',b'0',current_timestamp(),current_timestamp()),
+	('ALggysE','le minh huy','','0111111115',b'0',current_timestamp(), current_timestamp()),
+	('cchwDul','le minh huy','','0111111114',b'0',current_timestamp(), current_timestamp()),
+	('d5iGKPp','Nguyễn Hứa Hùng','TPHCM','0246813579',b'0',current_timestamp(), current_timestamp()),
+	('d71N9dN','lê hoàng anh','Đồng Tháp','0762672399',b'0',current_timestamp(), current_timestamp()),
+	('Er1E2t8','Bạch Minh Khôi','Khánh Hoà','0135792468',b'0',current_timestamp(), current_timestamp()),
+	('FDHVWyT','le minh huy','','0111111112',b'0',current_timestamp(), current_timestamp()),
+	('J6saKBZ','le minh huy','','0111111116',b'0',current_timestamp(), current_timestamp()),
+	('xSF4R5S','le minh huy','','0111111111',b'0',current_timestamp(), current_timestamp()),
+	('yli440d','Lê Minh Huy','Gia Lai','0987654321',b'0',current_timestamp(), current_timestamp()),
+	('zIFLqCG','le minh huy','','0111111113',b'0',current_timestamp(), current_timestamp());
+    
+INSERT INTO `heroku_a51da3167c7e5af`.`user_login` (id, username, password, role, updatedAt)
+VALUES
+    ('2eHuNfS', 'customer01', '1234567c', '3', current_timestamp()),
+    ('ALggysE', 'huytest05', '123456789h', '3', current_timestamp()),
+    ('cchwDul', 'huytest04', '123456789h', '3', current_timestamp()),
+    ('d5iGKPp', 'employee02', '1234567e', '2', current_timestamp()),
+    ('d71N9dN', 'admin_01', '1234567a', '1', current_timestamp()),
+    ('Er1E2t8', 'employee01', '1234567e', '2', current_timestamp()),
+    ('FDHVWyT', 'huytest02', '123456789h', '3', current_timestamp()),
+    ('J6saKBZ', 'huytest06', '123456789h', '3', current_timestamp()),
+    ('xSF4R5S', 'huytest01', '123456789h', '3', current_timestamp()),
+    ('yli440d', 'customer02', '1234567c', '3', current_timestamp()),
+    ('zIFLqCG', 'huytest03', '123456789h', '3', current_timestamp());
