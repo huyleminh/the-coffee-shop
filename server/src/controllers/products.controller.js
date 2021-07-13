cb2ec5850645b8b64fd7415396be74e848decb3d
binhhieutu <63572337+binhhieutu@users.noreply.github.com>
2021-07-13 23:49:44 +0700

    feature-apiGetAllCategory-done

---
+++ server/src/controllers/products.controller.js
@@ -0,0 +1,5 @@
+class ProductsController{
+    static 
+}
+
+export default ProductsController;
---
+++ server/src/routes/products.route.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import productsController from "../controllers/products.controller.js";
+const productsRouter=express.Router()
+export default productsRouter 
