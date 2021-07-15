a1fa7bc6650fbb43d923183c52149d5dc7b8ac7c
NguyenHuaHung <63572298+huahungnguyen121@users.noreply.github.com>
2021-07-16 01:09:32 +0700

    profile page

--- client/src/views/layouts/UserProfile/UserProfile.jsx
+++ client/src/views/layouts/UserProfile/UserProfile.jsx
@@ -1,11 +1,11 @@
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Layout } from "antd";
+import { Layout, Radio, Space } from "antd";
 import React, { useState } from "react";
 import "../../../assets/css/layouts/profile/UserProfile.css";
 import MenuImage from "../../../assets/images/menu.jpg";
+import defaultAvatar from "../../../assets/images/store-logo.png";
 import Hero from "../../../components/layouts/Hero";
-import defaultAvatar from "../../../assets/images/store-logo.png"
 
 const { Content } = Layout;
 
@@ -17,6 +17,17 @@ function UserProfile() {
         else setIsSidebarVisible(true);
     };
 
+    const toggleEdit = () => {
+        console.log("clicked");
+    };
+
+    const handleSave = () => {
+        console.log("clicked");
+    };
+
+    const handleCancel = () => {
+        console.log("clicked");
+    };
     const activeClassname = isSidebarVisible ? "active" : null;
 
     return (
@@ -25,12 +36,18 @@ function UserProfile() {
             <div className="wrapper profile">
                 <div className={`profile__sidebar ${activeClassname}`}>
                     <FontAwesomeIcon icon={faTimes} id="exit" onClick={toggleSidebar} />
-                    <img src={defaultAvatar} alt="default-avatar" className="avatar"></img>
+                    <img src={defaultAvatar} alt="defaultAvatar" className="avatar"></img>
                     <h1 className="username">Username</h1>
                     <ul className="nav__sidebar">
-                        <li><a href="/profile">Profile</a></li>
-                        <li><a href="#">Order History</a></li>
-                        <li><a href="#">Voucher</a></li>
+                        <li>
+                            <a href="/profile">Profile</a>
+                        </li>
+                        <li>
+                            <a href="#">Order History</a>
+                        </li>
+                        <li>
+                            <a href="#">Voucher</a>
+                        </li>
                     </ul>
                 </div>
 
@@ -44,16 +61,59 @@ function UserProfile() {
                 ) : null}
 
                 <div className="profile__manipulation">
-                    <div
-                        className={`profile__sidebar ${activeClassname}`}
-                        onClick={toggleSidebar}
-                    >
-                        <span>Filter</span>
+                    <div className={`profile__bar ${activeClassname}`} onClick={toggleSidebar}>
+                        <span></span>
                     </div>
-                    <div>test</div>
+                    <div className="myProfile">
+                        <h1>MY PROFILE</h1>
+                        <button className="" onClick={toggleEdit}>
+                            Edit
+                        </button>
+                    </div>
+                    <div className="profileForm">
+                        <form>
+                            <div className="profileForm_items">
+                                <label>Full name</label>
+                                <input type="text" />
+                            </div>
+                            <div className="profileForm_items">
+                                <label>Phone</label>
+                                <input type="text" />
+                            </div>
+                            <div className="profileForm_items">
+                                <label>Address</label>
+                                <div id="address_grid">
+                                    <input type="text" id="address"/>
+                                    <input type="text" id="ward"/>
+                                    <input type="text" id="district"/>
+                                </div>
+                            </div>
+                            <div className="profileForm_items">
+                                <label>City</label>
+                                <input type="text" />
+                            </div>
+                            <div className="profileForm_items">
+                                <label>Gender</label>
+                                <Radio.Group>
+                                    <Space direction="horizontal">
+                                        <Radio value="male">Male</Radio>
+                                        <Radio value="female">Female</Radio>
+                                    </Space>
+                                </Radio.Group>
+                            </div>
+                            <div className="profileForm_items">
+                                <button type="submit" onClick={handleSave}>
+                                    Save
+                                </button>
+                                <button type="submit" onClick={handleCancel}>
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </Content>
-    )
+    );
 }
 export default UserProfile;
