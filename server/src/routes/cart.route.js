879c971974dfdf4be5c4d132be8be4a8788368c9
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-07-19 22:22:05 +0700

    rename some variables and functions

--- server/src/routes/cart.route.js
+++ server/src/routes/cart.route.js
@@ -1,14 +1,14 @@
-import express from "express"
-import AuthorizationMiddleware from "../middlewares/authorization.middleware.js"
-import CartController from "../controllers/cart.controller.js"
+import express from "express";
+import AuthorizationMiddleware from "../middlewares/authorization.middleware.js";
+import CartController from "../controllers/cart.controller.js";
 
-const cartRouter = express.Router()
+const cartRouter = express.Router();
 
 cartRouter.post(
     "/add",
     AuthorizationMiddleware.verifyToken,
-    AuthorizationMiddleware.verifyInfoInToken,
+    AuthorizationMiddleware.verifyDataInToken,
     CartController.addProduct
-)
+);
 
-export default cartRouter
+export default cartRouter;
