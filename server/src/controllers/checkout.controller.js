f30362446f9bd956b92c5f5ebd985c3796cfedb1
PMHieu <19127141@student.hcmus.edu.vn>
2021-07-21 16:45:12 +0700

    feature/checkout-api-be-get- done

--- server/src/controllers/checkout.controller.js
+++ server/src/controllers/checkout.controller.js
@@ -1,30 +1,48 @@
 import Cart from "../utilities/database/entities/Cart.js";
-
+import UserInfo from "../utilities/database/entities/UserInfo.js";
 class CheckoutController {
+    static getUserInfo = async (req, res, next) => {
+        const userInfo = res.locals.userInfo;
+
+        res.send({
+            status: 200,
+            userInfo: {
+                name: userInfo.fullname,
+                address: userInfo.address,
+                phoneNumber: userInfo.phoneNumber,
+                gender: userInfo.gender,
+            },
+        });
+    };
+
     static confirmInfo = async (req, res, next) => {
         // Validate data in payload
-        const payload = res.locals.payload
+        const payload = res.locals.payload;
 
         if (payload.products.length === 0) {
-            res.send({ status: 404, message: "There is no product in this order" })
-            return
+            res.send({
+                status: 404,
+                message: "There is no product in this order",
+            });
+            return;
         }
 
-        const userInfo = res.locals.userInfo
-        const productsInCart = await Cart.getCartByUserId(userInfo.id)
-        const productIdList = productsInCart.map(product => product.id)
+        const userInfo = res.locals.userInfo;
+        const productsInCart = await Cart.getCartByUserId(userInfo.id);
+        const productIdList = productsInCart.map((product) => product.id);
         for (let product of payload.products) {
             if (!productIdList.includes(product.id)) {
                 res.send({
                     status: 404,
-                    message: "There is at least one product that does not exist in your cart"
-                })
-                return
+                    message:
+                        "There is at least one product that does not exist in your cart",
+                });
+                return;
             }
         }
 
-        next()
-    }
+        next();
+    };
 }
 
-export default CheckoutController
+export default CheckoutController;
