bfe622eb78f479e30c993df65ad4d4f8f8a575a5
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2021-07-28 17:56:58 +0700

    feature/order-history - create history table and sort by - done

--- client/src/views/layouts/HomePage.jsx
+++ client/src/views/layouts/HomePage.jsx
@@ -4,21 +4,19 @@ import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import "../../assets/css/layouts/HomePage.css";
 import Footer from "../../components/layouts/Footer";
 import Header from "../../components/layouts/Header";
-import Loading from "../../components/Loading";
 import PrivateRoute from "../../components/routes/PrivateRoute";
 import ScrollTopButton from "../../components/ScrollTopButton";
 import { HomePageEventsHandler } from "../../Events";
 import PageNotFound from "../errors/PageNotFound";
+import CartLayout from "./CartLayout/CartLayout";
 import CheckoutPage from "./Checkout/CheckoutPage";
 import LandingPage from "./LandingPage/LandingPage";
 import MenuPage from "./MenuPage/MenuPage";
-import UserProfile from "./UserProfile/UserProfile";
-import CartLayout from "./CartLayout/CartLayout";
+import ProfilePage from "./UserProfile/ProfilePage";
 import Wishlist from "./wishlist/Wishlist";
 
 function HomePage() {
     const history = useHistory();
-    const [isLoading, setIsLoading] = useState(true);
     const [userStatus, setUserStatus] = useState({ isLogin: false, role: 3 });
 
     const clearLocal = () => {
@@ -43,7 +41,7 @@ function HomePage() {
     });
 
     useEffect(() => {
-        const checkLogin = async () => {
+        const checkLogin = () => {
             const user = JSON.parse(localStorage.getItem("user"));
             if (user) {
                 const { token, expiredIn, role } = user;
@@ -53,8 +51,8 @@ function HomePage() {
                 } else {
                     if (new Date().valueOf() > expiredIn) {
                         alert("Your session has expired.");
-                        history.push("/login");
-                        return;
+                        clearLocal();
+                        setUserStatus({ isLogin: false, role: 3 });
                     } else {
                         setUserStatus({ isLogin: true, role: role });
                     }
@@ -63,36 +61,19 @@ function HomePage() {
                 clearLocal();
                 setUserStatus({ isLogin: false, role: 3 });
             }
-
-            setTimeout(() => {
-                setIsLoading(false);
-            }, 800);
         };
 
-        if (isLoading) checkLogin();
+        checkLogin();
         // eslint-disable-next-line
     }, []);
 
-    if (isLoading)
-        return (
-            <Loading
-                style={{
-                    width: "100%",
-                    height: "100vh",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            />
-        );
-    else
-        return (
-            <div>
-                <Header userStatus={userStatus} />
+    return (
+        <div>
+            <Header userStatus={userStatus} />
 
                 <Layout className="container">
                     <Switch>
-                        <PrivateRoute exact path="/profile" component={UserProfile} />
+                        <PrivateRoute path="/profile" component={ProfilePage} />
                         <Route exact path="/menu" component={MenuPage} />
                         <Route exact path="/cart" component={CartLayout} />
                         <Route exact path="/wishlist" component={Wishlist} />
@@ -105,10 +86,12 @@ function HomePage() {
                         </Route>
                     </Switch>
                 </Layout>
-                <ScrollTopButton />
-                <Footer />
-            </div>
-        );
+
+            <ScrollTopButton />
+
+            <Footer />
+        </div>
+    );
 }
 
 export default HomePage;
