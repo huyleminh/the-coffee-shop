2562e3195bbb87e5fdcfe91a7143bbae3d0cb118
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2021-07-29 17:53:59 +0700

    cart remove

--- client/src/views/layouts/CartLayout/CartLayout.jsx
+++ client/src/views/layouts/CartLayout/CartLayout.jsx
@@ -9,8 +9,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { LoadingOutlined } from "@ant-design/icons";
 import CartImage from "../../../assets/images/landing-hero.jpg";
 import ProductItem from "../MenuPage/Product/ProductItem";
-import CartAPI from "../../../services/Cart/CartAPI.js"
-import { Storage } from "../../../utilities/firebase/FirebaseConfig.js"
+import CartAPI from "../../../services/Cart/CartAPI.js";
+import { Storage } from "../../../utilities/firebase/FirebaseConfig.js";
 const { Content } = Layout;
 
 // Handle items which are selected
@@ -26,8 +26,8 @@ function CartLayout() {
     const [images, setImages] = useState([]);
 
     const [cart, setCart] = useState(() => {
-        const cartLocal = JSON.parse(localStorage.getItem("cart"))
-        return cartLocal ? cartLocal : []
+        const cartLocal = JSON.parse(localStorage.getItem("cart"));
+        return cartLocal ? cartLocal : [];
     });
 
     const handleGoMenu = () => {
@@ -38,32 +38,19 @@ function CartLayout() {
         history.push("/checkout");
     };
 
-    const handleRemoveItem = (key) => {
-        setIsSending(true);
-        setSelectedItem([key]);
-        setIsRemoving(true);
-        setIsSending(false);
-    };
+    // const handleRemoveItem = (key) => {
+    //     setIsSending(true);
+    //     setSelectedItem([key]);
+    //     setIsRemoving(true);
+    //     setIsSending(false);
+    // };
 
     const handleSelected = (keys) => {
-        //console.log("keys: ", keys);
-        setSelectedItem([keys]);
-
-        //let totalTemp = 0;
-        for (let key of keys) {
-            //console.log("SELECTED: ", key);
-            for (let item of cart) {
-                //console.log(referralItem);
-                //console.log(item["key"]);
-                if (key === item["key"]) {
-                    //console.log("REFERRAL: ", item);
-                    //totalTemp += item["total"];
-                }
-            }
+        for (let i = 0; i < 1; i++) {
+            setSelectedItem([keys]);
         }
-
-        //setTotalMoney(totalTemp);
-        //console.log("TOTAL: ", totalTemp);
+        //setSelectedItem([keys]);
+        console.log(selectedItem);
     };
 
     const handleToBuyItem = () => {
@@ -80,14 +67,19 @@ function CartLayout() {
     };
 
     const handleQuantity = (item, value) => {
-        //console.log(item);
+        // console.log("ITEM: ", item);
         // const cloneData = [...cart];
+        // console.log("CLONE DATA: ", cloneData);
         // handleToBuyItem();
         // for (let clone of cloneData) {
-        //     if (clone["key"] === item["key"]) {
-        //         console.log(clone["key"]);
+        //     console.log(clone["product"]["id"]);
+        //     console.log(item["key"]);
+        //     if (clone["product"]["id"] === item["key"]) {
+        //         console.log(clone["id"]);
         //         const base =
-        //             clone["price"]["price"] - clone["price"]["price"] * clone["price"]["discount"];
+        //             clone["product"]["price"] -
+        //             clone["product"]["price"] * clone["discount"]["percent"];
+        //         console.log(base);
         //         setTotalMoney(totalMoney - clone["total"]);
         //         clone["quantity"] = value;
         //         clone["total"] = base * value;
@@ -103,31 +95,117 @@ function CartLayout() {
         // setCart(cloneData);
     };
 
-    const removeSelectedItem = () => {
+    // const removeSelectedItem = () => {
+    //     console.log("CART: ", cart);
+    //     //console.log("CART TABLE: ", cartTable);
+
+    //     const newCart = [];
+    //     const removeItem = [];
+    //     let isModified = false;
+    //     let isDeleted = false;
+
+    //     console.log("SELECTED: ", selectedItem);
+    //     for (let item of cart) {
+    //         isDeleted = false;
+    //         for (let key of selectedItem[0]) {
+    //             console.log(item);
+    //             if (item["key"] === key) {
+    //                 isModified = true;
+    //                 removeItem.push(item);
+    //                 isDeleted = true;
+    //             }
+    //         }
+    //         if (!isDeleted) {
+    //             newCart.push(item);
+    //         }
+    //     }
+    //     console.log("NEW CART: ", newCart);
+
+    //     if (isModified) {
+    //         setCart(newCart);
+    //     }
+    // };
+    const handleRemoveItem = (key) => {
+        setIsSending(true);
+        // setSelectedItem([key]);
+        // setIsRemoving(true);
+        removeSelectedItem([key]);
+    };
+
+    const removeSelectedItem = async (params) => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        const cart = JSON.parse(localStorage.getItem("cart"));
         const newCart = [];
         const removeItem = [];
-        let isModified = false;
         let isDeleted = false;
-        console.log("CART: ", cart);
-        console.log("SELECTED: ", selectedItem);
+        let deleted = false;
         for (let item of cart) {
-            isDeleted = false;
-            for (let key of selectedItem[0]) {
-                console.log(item);
-                if (item["key"] === key) {
-                    isModified = true;
-                    removeItem.push(item);
+            deleted = false;
+            for (let key of params) {
+                if (item["product"]["id"] === key) {
                     isDeleted = true;
+                    removeItem.push(item);
+                    deleted = true;
                 }
             }
-            if (!isDeleted) {
+            if (!deleted) {
                 newCart.push(item);
             }
         }
-        console.log("NEW CART: ", newCart);
 
-        if (isModified) {
-            setCart(newCart);
+        if (!user || !user.token) {
+            localStorage.removeItem("user");
+            if (isDeleted) {
+                alert("Remove successfully.");
+                localStorage.setItem("cart", JSON.stringify(newCart));
+                setCart(newCart);
+                setIsSending(false);
+            }
+        } else {
+            try {
+                const removeItemPromise = removeItem.map((item) => {
+                    return CartAPI.deleteProduct(user.token, item.product.id);
+                });
+                const response = await Promise.all(removeItemPromise);
+                let countNotExist = 0;
+                for (let item of response) {
+                    if (item.status === 200) {
+                        console.log("success");
+                    } else if (item.status === 404) {
+                        if (item.message === "This user does not exist") {
+                            localStorage.removeItem("user");
+                        } else {
+                            console.log(item.message);
+                            countNotExist += 1;
+                        }
+                    } else if (item.status === 401 || item.status === 403) {
+                        localStorage.removeItem("user");
+                    }
+                }
+                if (isDeleted) {
+                    alert("Remove successfully.");
+                    if (countNotExist !== 0) {
+                        alert(`${countNotExist} item(s) does not exist in your cart.`);
+                    }
+                    localStorage.setItem("cart", JSON.stringify(newCart));
+                    setCart(newCart);
+                    setIsSending(false);
+                }
+            } catch (error) {
+                console.log(error);
+                alert("Something went wrong.");
+            }
+        }
+    };
+
+    const handleRemoveSelected = () => {
+        if (selectedItem.length === 0) {
+            alert("No item is being selected.\nPlease select item(s) and try again.");
+        } else {
+            setIsSending(true);
+            // setIsRemoving(true);
+            removeSelectedItem(selectedItem);
+            setSelectedItem([]);
         }
     };
 
@@ -136,58 +214,59 @@ function CartLayout() {
         alert("ADDED TO WISHLIST (FAKE)");
     };
 
-    const handleRemoveSelected = () => {
-        if (selectedItem.length === 0) {
-            alert("No item is being selected.\nPlease select item(s) and try again.");
-        } else {
-            setIsSending(true);
-            setIsRemoving(true);
-            setIsSending(false);
-        }
-    };
+    // const handleRemoveSelected = () => {
+    //     if (selectedItem.length === 0) {
+    //         alert("No item is being selected.\nPlease select item(s) and try again.");
+    //     } else {
+    //         setIsSending(true);
+    //         setIsRemoving(true);
+    //         setIsSending(false);
+    //     }
+    // };
 
     useEffect(() => {
         const fetchCart = async () => {
-            const user = JSON.parse(localStorage.getItem("user"))
+            const user = JSON.parse(localStorage.getItem("user"));
 
             if (!user || !user.token) {
-                localStorage.removeItem("user")
-                setIsLoading(false)
+                localStorage.removeItem("user");
+                setIsLoading(false);
             } else {
                 try {
-                    const response = await CartAPI.getCart(user.token)
+                    const response = await CartAPI.getCart(user.token);
                     if (response.status === 200) {
-                        const resCart = response.data
-                        setCart(resCart)
-                        localStorage.setItem("cart", JSON.stringify(resCart))
+                        const resCart = response.data;
+                        setCart(resCart);
+                        localStorage.setItem("cart", JSON.stringify(resCart));
                     } else {
-                        localStorage.removeItem("user")
-                        const cartLocal = localStorage.getItem("cart")
-                        setCart(cartLocal ? cartLocal : [])
+                        localStorage.removeItem("user");
+                        const cartLocal = localStorage.getItem("cart");
+                        setCart(cartLocal ? cartLocal : []);
                     }
                 } catch (error) {
-                    alert("Something went wrong.")
+                    alert("Something went wrong.");
                 }
             }
-        }
+        };
 
-        fetchCart()
-    }, [])
+        fetchCart();
+        handleSelected();
+    }, []);
 
     useEffect(() => {
         const fetchImages = async () => {
-            const imagePromises = cart.map(item => {
-                const image = item.product.image ? item.product.image : "latte.jpg"
-                return Storage.ref(`products/${image}`).getDownloadURL()
-            })
+            const imagePromises = cart.map((item) => {
+                const image = item.product.image ? item.product.image : "latte.jpg";
+                return Storage.ref(`products/${image}`).getDownloadURL();
+            });
 
-            const images = await Promise.all(imagePromises)
-            setImages(images)
-            setIsLoading(false)
-        }
+            const images = await Promise.all(imagePromises);
+            setImages(images);
+            setIsLoading(false);
+        };
 
-        fetchImages()
-    }, [cart])
+        fetchImages();
+    }, [cart]);
 
     useEffect(() => {
         if (isRemoving) {
@@ -199,16 +278,16 @@ function CartLayout() {
         }
     }, [isRemoving]);
 
-    useEffect(() => {
-        handleToBuyItem();
-    }, [cart]);
+    // useEffect(() => {
+    //     handleToBuyItem();
+    // }, [cart]);
 
     const cartTable = cart.map((item, index) => {
         const price = {
             discount: item.discount ? item.discount.percent : 0,
-            price: item.product.price
-        }
-        const total = item.quantity * (price.price * (1 - price.discount))
+            price: item.product.price,
+        };
+        const total = item.quantity * (price.price * (1 - price.discount));
 
         return {
             key: item.product.id,
@@ -216,14 +295,14 @@ function CartLayout() {
             image: {
                 src: images[index],
                 width: "100px",
-                height: "100px"
+                height: "100px",
             },
             price,
             quantity: item.quantity,
             total,
             action: "wishlist",
-        }
-    })
+        };
+    });
 
     return (
         <Content>
