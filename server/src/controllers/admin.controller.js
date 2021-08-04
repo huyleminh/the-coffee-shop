3b0a887f944321a94a3c46bac25aa107e41c2a90
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2021-08-04 16:22:02 +0700

    feature/admin-api-be - setup basic code - done

---
+++ server/src/controllers/admin.controller.js
@@ -0,0 +1,55 @@
+class AdminController {
+    static getAllProducts = async (req, res) => {
+        // Các dữ liệu có sẵn
+        // res.locals.userInfo
+        // res.locals.userLogin
+        // res.locals.payload
+        // res.locals.params
+
+        // Các lỗi đã xử lý
+        // { status: 404, message: "This user does not exist" }
+        // { status: 401, message: "Lack of information in the token" }
+        // { status: 403, message: <depend on error> }
+    }
+
+    static createProduct = async (req, res) => {
+        // Các dữ liệu có sẵn
+        // res.locals.userInfo
+        // res.locals.userLogin
+        // res.locals.payload
+        // res.locals.params
+
+        // Các lỗi đã xử lý
+        // { status: 404, message: "This user does not exist" }
+        // { status: 401, message: "Lack of information in the token" }
+        // { status: 403, message: <depend on error> }
+    }
+
+    static editProduct = async (req, res) => {
+        // Các dữ liệu có sẵn
+        // res.locals.userInfo
+        // res.locals.userLogin
+        // res.locals.payload
+        // res.locals.params
+
+        // Các lỗi đã xử lý
+        // { status: 404, message: "This user does not exist" }
+        // { status: 401, message: "Lack of information in the token" }
+        // { status: 403, message: <depend on error> }
+    }
+
+    static deleteProduct = async (req, res) => {
+        // Các dữ liệu có sẵn
+        // res.locals.userInfo
+        // res.locals.userLogin
+        // res.locals.payload
+        // res.locals.params
+
+        // Các lỗi đã xử lý
+        // { status: 404, message: "This user does not exist" }
+        // { status: 401, message: "Lack of information in the token" }
+        // { status: 403, message: <depend on error> }
+    }
+}
+
+export default AdminController
