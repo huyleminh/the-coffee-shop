d37e4d994310025f20efa26db2f3f16f3e57e706
PMHieu <19127141@student.hcmus.edu.vn>
2021-09-08 18:01:09 +0700

    format file category controller
    format code

--- server/src/controllers/categories.controller.js
+++ server/src/controllers/categories.controller.js
@@ -1,15 +1,14 @@
 import Category from "../utilities/database/entities/Category.js";
 
 class CategoriesController {
-   static getAll = async (req, res) => {
-      const data = await Category.getAll();
-      const param = req.query;
+    static getAll = async (req, res) => {
+        const data = await Category.getAll();
+        const param = req.query;
 
-      if (Object.keys(param).length !== 0)
-         res.send({ status: 404, message: "Page not found" });
-      else
-         res.send({ status: 200, data });
-   };
+        if (Object.keys(param).length !== 0)
+            res.send({ status: 404, message: "Page not found" });
+        else res.send({ status: 200, data });
+    };
 }
 
 export default CategoriesController;
